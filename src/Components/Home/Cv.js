edf2902682587fffbe26cd7481f88ac01ec8c0fe
ttelmo1 <telmobsj@gmail.com>
2022-12-27 19:29:18 -0300

    corrigindo link de download cv

--- src/Components/Home/Cv.js
+++ src/Components/Home/Cv.js
@@ -3,7 +3,7 @@ import cv from "./cv.svg";
 export default function Cv(){
     return(
         <div className="cv">
-            <a href="./cv_telmo.pdf" download="cv_telmo.pdf">
+            <a href="https://drive.google.com/file/d/1GK_bEUPYeL8E00kYn4J-DAOl3AyLOxlj/view" download="cv_telmo">
                 <img src={cv} alt="cv" />
                 <p>Faça download do meu CV</p>
             </a>
