e392ed6985becc3528bae0578abfdb1f6334786d
Telmo Jr <108095100+ttelmo1@users.noreply.github.com>
2023-04-20 10:05:07 -0300

    Update Cv.js

--- src/Components/Home/Cv.js
+++ src/Components/Home/Cv.js
@@ -3,7 +3,7 @@ import cv from "./cv.svg";
 export default function Cv(){
     return(
         <div className="cv">
-            <a href="https://drive.google.com/file/d/1GK_bEUPYeL8E00kYn4J-DAOl3AyLOxlj/view" download="cv_telmo">
+            <a href="https://drive.google.com/file/d/1sgtD7c7E8nCGrzlmm4wI4IM4dRRgTqJW/view" download="cv_telmo">
                 <img src={cv} alt="cv" />
                 <p>Faça download do meu CV</p>
             </a>
